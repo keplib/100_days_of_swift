9fa7b3558e728d3fe066e00da002069ff91ed269
keplib <keplib@gmail.com>
2024-10-11 00:20:12 +0200

    MVVM implemented

--- GuessTheFlag/GuessTheFlag/GameModel.swift
+++ GuessTheFlag/GuessTheFlag/GameModel.swift
@@ -10,12 +10,17 @@ import Foundation
 
 struct GameModel<FlagContent> {
     
+    // These are the variables the game logice needs to know about
     var flags: Array<FlagContent>
     var userPoints: Int
     var correctAnswer: Int
     var usersChoice: Int
     
     init(countryNames: Array<FlagContent>) {
+        // Itt eleg lenne csak az elso 3 kartyat mutatni
+        // flagsToShow = flags[0..3]
+        // a UI-nak tobb info nem kell
+        // Plusz, countryNames.shuffled(), kulonben mindig ugyanugy indul majd a jatek, ugyanazzal a 3 kartyaval
         flags = countryNames
         userPoints = 0
         correctAnswer = Int.random(in: 0...2)
@@ -23,6 +28,8 @@ struct GameModel<FlagContent> {
     }
     
     mutating func askQuestion () {
+        //itt is, csak az elso 3 kartya
+        // flagsToShow = flags.shuffle()[0..3]
         flags.shuffle()
         correctAnswer = Int.random(in: 0...2)
     }
--- GuessTheFlag/GuessTheFlag/ViewModel.swift
+++ GuessTheFlag/GuessTheFlag/ViewModel.swift
@@ -9,21 +9,39 @@ import Foundation
 
 class ViewModel: ObservableObject {
     
+    // This is the content for the game that will be sent down to the initializer of the GameModel
     private static let countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US"]
     
-    @Published var showAlert: Bool = false
-    @Published private var game = createFlagGame()
-    @Published var popupHeader: String = ""
-    
-    
+    // And this function creates an instance of GameModel
     private static func createFlagGame () -> GameModel<String> {
         return GameModel(countryNames: countries)
     }
     
+    // Create an instance of FlagGame that is going to be only available here so the VM can interact with the model
+    private var game = createFlagGame()
+    
+    //These are the variables from the model that the VM makes available for the UI (read only)
     var flags: Array<String> {
         return game.flags
-       }
+    }
     
+    // Itt at lehetne terni
+    // var flagsToShow: Array<String> { return game.flagsToShow }
+    
+    var correctAnswer: Int {
+        return game.correctAnswer
+    }
+    
+    var userScore: Int {
+        return game.userPoints
+    }
+    
+    //These on the other hand are variables that the UI need (e.g. showAlert) that are NOT part of the model. So these only exist as a part of the VM
+    @Published var showAlert: Bool = false
+    @Published var popupHeader: String = ""
+    
+   
+   //And here are the functions to interact with the model. These function can be triggered from the UI as vm.getQuestion() and then the VM triggers the model's internal methods
     func getQuestion() {
         game.askQuestion()
     }
@@ -33,8 +51,10 @@ class ViewModel: ObservableObject {
         
         if result {
             popupHeader = "Correct"
+            
         } else {
             popupHeader = "Wrong"
         }
+        showAlert = true
     }
 }
