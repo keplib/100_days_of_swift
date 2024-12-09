04ffc977d71bb355a746582d142fff0095d46ea8
keplib <keplib@gmail.com>
2024-12-09 20:04:44 +0100

    create basic layout for mission cards

--- Moonshot/Moonshot/Models/Mission.swift
+++ Moonshot/Moonshot/Models/Mission.swift
@@ -8,13 +8,28 @@
 import Foundation
 
 struct Mission: Codable, Identifiable {
+    
+    struct Crew: Codable {
+        let name: String
+        let role: String
+    }
+    
     let id: Int
-    let launchDate: String?
+    let launchDate: Date?
     let crew: [Crew]
     let description: String
+    
+    var displayName: String {
+        "Apollo \(id)"
+    }
+    
+    var image: String {
+        "apollo\(id)"
+    }
+    
+    var formattedLaunchDate : String {
+        launchDate?.formatted(date: .abbreviated, time: .omitted) ?? "N/A"
+    }
 }
 
-struct Crew: Codable {
-    let name: String
-    let role: String
-}
+
