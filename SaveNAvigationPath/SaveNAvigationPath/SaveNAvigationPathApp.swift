63a339092190057de5f899ccc320fed528df51c2
keplib <keplib@gmail.com>
2025-02-03 22:11:19 +0100

    Saving NavigationPath to memory

---
+++ SaveNAvigationPath/SaveNAvigationPath/SaveNAvigationPathApp.swift
@@ -0,0 +1,17 @@
+//
+//  SaveNavigationPathApp.swift
+//  SaveNavigationPath
+//
+//  Created by Balázs Képli on 3/2/25.
+//
+
+import SwiftUI
+
+@main
+struct SaveNavigationPathApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
