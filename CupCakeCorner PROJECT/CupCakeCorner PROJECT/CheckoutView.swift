bf6ce63cd419acea9c640ec06eaf7719225da76c
keplib <keplib@gmail.com>
2025-02-09 16:51:15 +0100

    Implement costs and CheckOut View

--- CupCakeCorner PROJECT/CupCakeCorner PROJECT/CheckoutView.swift
+++ CupCakeCorner PROJECT/CupCakeCorner PROJECT/CheckoutView.swift
@@ -12,7 +12,23 @@ struct CheckoutView: View {
     var order: Order
     
     var body: some View {
-        Text("CheckoutView")
+        ScrollView {
+            VStack {
+                AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale:3) { image in
+                    image
+                        .resizable().scaledToFit()
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+                Text("Your total cost is \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+                Button("Place order") {}
+            }
+        }
+        .navigationTitle("Checkout")
+        .navigationBarTitleDisplayMode(.inline)
+        .scrollBounceBehavior(.basedOnSize)
     }
 }
 
