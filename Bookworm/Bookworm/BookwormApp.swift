ac1060be423f6083318372770dff85415cec0a0d
keplib <keplib@gmail.com>
2025-04-04 14:16:35 +0200

    bookworm starter project

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Balázs Képli on 4/4/25.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
