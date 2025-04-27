487382d83c21e759af2177a062145059ce0ad1d4
keplib <keplib@gmail.com>
2025-04-27 17:25:44 +0200

    starter project

---
+++ SwiftDataAdvanced/SwiftDataAdvanced/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftDataAdvanced
+//
+//  Created by Balázs Képli on 27/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftDataAdvanced/SwiftDataAdvanced/SwiftDataAdvancedApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataAdvancedApp.swift
+//  SwiftDataAdvanced
+//
+//  Created by Balázs Képli on 27/4/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataAdvancedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
