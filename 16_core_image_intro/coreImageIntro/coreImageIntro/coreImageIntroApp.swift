f486e9c746b9f98e52c0e05ba32a743d5456846c
keplib <keplib@gmail.com>
2025-06-24 17:12:40 +0200

    day62_starter_commit

---
+++ 16_core_image_intro/coreImageIntro/coreImageIntro/coreImageIntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  coreImageIntroApp.swift
+//  coreImageIntro
+//
+//  Created by Balázs Képli on 24/6/25.
+//
+
+import SwiftUI
+
+@main
+struct coreImageIntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
