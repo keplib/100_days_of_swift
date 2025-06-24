ef1478321d12cc94e7cb0f1a87427cc7617aecbf
keplib <keplib@gmail.com>
2025-06-24 18:25:54 +0200

    add different filters to image

--- 16_core_image_intro/coreImageIntro/coreImageIntro/ContentView.swift
+++ 16_core_image_intro/coreImageIntro/coreImageIntro/ContentView.swift
@@ -6,16 +6,39 @@
 //
 
 import SwiftUI
+import CoreImage
+import CoreImage.CIFilterBuiltins
 
 struct ContentView: View {
+    @State private var image: Image?
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            image?
+                .resizable()
+                .scaledToFit()
         }
-        .padding()
+        .onAppear(perform: loadImage)
+    }
+    
+    func loadImage() {
+        
+        let loadImage = UIImage(resource: .example)
+        let beginImage = CIImage(image: loadImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.twirlDistortion()
+        currentFilter.inputImage = beginImage
+        currentFilter.radius = 1000
+        currentFilter.center = CGPoint(x: loadImage.size.width / 2, y: loadImage.size.height / 2)
+
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        
+        image = Image(uiImage: uiImage)
     }
 }
 
