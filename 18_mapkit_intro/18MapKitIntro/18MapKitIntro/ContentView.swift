691f8e7b4051de026e17c2503e6bae88ec5e6bd6
keplib <keplib@gmail.com>
2025-08-09 16:55:22 +0200

    MapKit Intro

--- 18_mapkit_intro/18MapKitIntro/18MapKitIntro/ContentView.swift
+++ 18_mapkit_intro/18MapKitIntro/18MapKitIntro/ContentView.swift
@@ -6,16 +6,89 @@
 //
 
 import SwiftUI
+import MapKit
+
+struct Location: Identifiable {
+    let id = UUID()
+    var name: String
+    var coordinate: CLLocationCoordinate2D
+}
 
 struct ContentView: View {
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
+    
+    @State private var position = MapCameraPosition.region(
+        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.507222, longitude: -0.1275), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+    )
     var body: some View {
+        //        VStack {
+        //            Map(position: $position)
+        //                .mapStyle(.hybrid)
+        //                .onMapCameraChange {CameraChange in
+        //                    print(CameraChange.region)
+        //                }
+        //
+        //            HStack {
+        //                Button("Paris") {
+        //                    position = MapCameraPosition.region(
+        //                        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        //                    )
+        //                }
+        //
+        //                Button("Tokyo") {
+        //                    position = MapCameraPosition.region(
+        //                        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+        //                    )
+        //                }
+        //            }
+        //        }
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            //            Map {
+            //                ForEach(locations) { location in
+            //                    //Marker(location.name, coordinate: location.coordinate)
+            //                    Annotation(location.name, coordinate: location.coordinate) {
+            //                        Text(location.name)
+            //                            .font(.headline)
+            //                            .padding()
+            //                            .background(.blue)
+            //                            .foregroundStyle(.white)
+            //                            .clipShape(.capsule)
+            //                    }
+            //                    .annotationTitles(.hidden)
+            //                }
+            //            }
+            
+            MapReader { proxy in
+                Map()
+                    .onTapGesture { position in
+                        if let coordinate = proxy.convert(position, from: .local) {
+                            print(coordinate)
+                        }
+                    }
+            }
+            
+            HStack {
+                Button("Paris") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 48.8566, longitude: 2.3522), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+                    )
+                }
+                
+                Button("Tokyo") {
+                    position = MapCameraPosition.region(
+                        MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 35.6897, longitude: 139.6922), span: MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1))
+                    )
+                }
+            }
         }
-        .padding()
+        
+        
     }
 }
 
